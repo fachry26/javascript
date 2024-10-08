439320d3917326a808b4d5760ef99fc57674e53d
fachry26 <fachryanbia@gmail.com>
2024-10-09 02:34:44 +0700

    practice stop at 2:24 am

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-function pertambahan (p1,p2){
-    return p1*p2
+// Function to multiply two numbers
+
+
+function pertambahan(p1, p2) {
+    return p1 * p2;
 }
+
+
+//document.addEventListener('DOMContentLoaded',function(){
+var kalimat = "Belajar js";
+    // Display the string in "hasil"
+
+    // String: Num
+    var Setring = "Num";
+    document.getElementById("hasil2").innerHTML = Setring;
+
+    // Array: buahbuahan
+    var buahbuahan = ["pisang", "semangka", "korma"];
+    document.getElementById("hasil3").innerHTML = buahbuahan[0]; // Display array elements as a string
+//})
+
+function mobli() {
+    let mobel = [{ nama: "volvo", warna: "kuning" }, { nama: "bmw", warna: "kuning busuk" }];
+document.getElementById("mobil").innerHTML = mobel[0].nama;
+mobli() }
